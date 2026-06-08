81887b4310955a3436d0cc110b4e7fd55579e87d
Melanie2649-ai <a20222649@pucp.edu.pe>
2026-06-08 13:55:18 -0500

    Tarea individual 2

--- template_hw_spatial.R
+++ template_hw_spatial.R
@@ -1,12 +1,12 @@
 rm(list = ls())
 #setwd("")
 
-gpklink="uruguay_maps.gpkg"
+gpklink="combined_americas.gpkg"
 # jsonlink
 
 library(sf)
 
-map1=read_sf(gpklink, layer = "uruguay_ddm")
+map1=read_sf(gpklink, layer = "mobiles_ddm")
 
 library(ggplot2)
 map1gg=ggplot() + theme_bw() + geom_sf(data = map1, color='red')
@@ -14,16 +14,17 @@ map1gg
 
 saveRDS(map1gg,file='map1.rds')
 
-map2=read_sf(gpklink, layer = "uruguay_psm")
+map2=read_sf(gpklink, layer = "mobiles_psm")
 
 map2gg=ggplot() + theme_void() + geom_sf(data = map2,
                                          aes(size=size))
 map2gg
 saveRDS(map2gg,file='map2.rds')
 
-map3=read_sf(gpklink, layer = "continent")
-map3gg=ggplot() + theme_void() + geom_sf(data = map3,
-                                         aes(fill=Población))
-map3gg
+map4=read_sf(gpklink, layer = "continent")
+map4gg=ggplot() + theme_void() + geom_sf(data = map4,
+                                         aes(fill= mobiles_q5_cat))
+map4gg
 
-saveRDS(map1gg,file='map3.rds')
+saveRDS(map1gg,file='map4.rds')
+
